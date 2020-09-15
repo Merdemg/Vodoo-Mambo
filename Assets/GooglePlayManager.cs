4e96f05b199e55166166197aa66ebcfc31391cf6
Erdem Gunay <merdemgunay88@gmail.com>
2020-09-14 20:08:57 -0400

    Google Play manager initial script.

--- Assets/GooglePlayManager.cs
+++ Assets/GooglePlayManager.cs
@@ -1,18 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
 
 public class GooglePlayManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.Activate();
+
+        SignIn();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void SignIn()
+    {
+        Social.localUser.Authenticate(success => { });
+    }
+
+    public void UnlockAchievement(string id)
+    {
+        Social.ReportProgress(id, 100, success => { });
+    }
+
+    public void AddScoreToLeaderboard(string leaderboardId, long score)
+    {
+        Social.ReportScore(score, leaderboardId, succcess => { });
+    }
+
+    public void ShowLeaderboard()
+    {
+        Social.ShowLeaderboardUI();
     }
 }
